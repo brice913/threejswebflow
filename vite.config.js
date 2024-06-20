94655790a0b1fc8199d1e5fe71c9b777512486af
brice913 <129128084+brice913@users.noreply.github.com>
2024-06-21 00:04:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,19 @@
-import { defineConfig } from 'vite'
-import glsl from 'vite-plugin-glsl';
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+import path from "path";
 
-// vite.config.js
 export default defineConfig({
   plugins: [glsl()],
-  server: {
-    host: 'localhost',
-    cors: '*',
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-    },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'three': path.resolve(__dirname, 'node_modules/three')
+    }
   },
   build: {
-    minify: true,
-    manifest: true,
+    outDir: 'dist',
     rollupOptions: {
       input: './src/main.js',
-      output: {
-        format: 'umd',
-        entryFileNames: 'main.js',
-        esModule: false,
-        compact: true,
-        globals: {
-          jquery: '$',
-        },
-      },
-      external: ['jquery'],
     },
   },
-})
+});
